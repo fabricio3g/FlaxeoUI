cee314465a7f1bac43a6b269f1130dd01d5bc814
Fabricio Medrano <48396639+fabricio3g@users.noreply.github.com>
2026-01-06 08:05:48 -0300

    chore: update build config and tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,23 @@ import tseslint from '@electron-toolkit/eslint-config-ts'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 
 export default defineConfig(
-  { ignores: ['**/node_modules', '**/dist', '**/out'] },
+  {
+    ignores: [
+      '**/node_modules',
+      '**/dist',
+      '**/out',
+      '**/server.js',
+      '**/create-folder-structure.js'
+    ]
+  },
   tseslint.configs.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  {
+    rules: {
+      // Allow require for server.js (CommonJS)
+      '@typescript-eslint/no-require-imports': 'off',
+      // Disable explicit function return type for complex composables
+      '@typescript-eslint/explicit-function-return-type': 'off'
+    }
+  }
 )
