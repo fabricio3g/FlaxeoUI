8faec2238af9caf6003fe7e4d61db3c68b50a7cb
Fabricio Medrano <48396639+fabricio3g@users.noreply.github.com>
2026-01-06 08:11:49 -0300

    chore: add build-server script

---
+++ build-server.js
@@ -0,0 +1,28 @@
+const esbuild = require('esbuild')
+
+console.log('Building server...')
+
+esbuild
+  .build({
+    entryPoints: ['server.js'],
+    bundle: true,
+    platform: 'node',
+    target: 'node18',
+    outfile: 'out/server/index.js',
+    external: [
+      // Only externalize native modules that can't be bundled
+      'electron',
+      '@ngrok/ngrok',
+      'cloudflared'
+    ],
+    format: 'cjs',
+    sourcemap: false,
+    minify: true
+  })
+  .then(() => {
+    console.log('Server build complete: out/server/index.js')
+  })
+  .catch((err) => {
+    console.error('Server build failed:', err)
+    process.exit(1)
+  })
